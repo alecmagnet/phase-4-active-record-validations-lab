4c71fbd46bc8eee85362b8505a2e2bb79296ed51
Alec Magnet <amagnet@gmail.com>
2021-09-01 16:50:10 -0400

    added regex solution in post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+	validates :title, presence: true
+	validates :content, length: { minimum: 250 }
+	validates :summary, length: { maximum: 250 }
+	validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+	validate :must_be_clickbait
+
+	def must_be_clickbait
+		unless title.present? && title.to_s.match?(/Believe|Secret|Top \d|Guess/)
+			errors.add(:title, "Title must be sufficiently clickbait-y!")
+		end
+	end
+
 end
